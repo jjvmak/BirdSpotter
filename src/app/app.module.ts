a38b999709ffc7bd20a77d95998ac300239689b5
Joonas Mäkinen <makinen.jns@gmail.com>
2017-11-23 18:36:53 +0200

    Created sightings component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { SightingsComponent } from './sightings/sightings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SightingsComponent
   ],
   imports: [
     BrowserModule,
