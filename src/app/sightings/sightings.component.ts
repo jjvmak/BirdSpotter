8c2ca9b3daf5668dd5cfa297f447dfad9aa73eb3
Joonas Mäkinen <makinen.jns@gmail.com>
2017-11-24 13:52:51 +0200

    Draft for sorting sighting instances. Job is done by by calculating date in one year and sorting is done by insertion sort.

--- src/app/sightings/sightings.component.ts
+++ src/app/sightings/sightings.component.ts
@@ -19,30 +19,52 @@ export class SightingsComponent implements OnInit {
         this.ln = Object.keys(resp.body).length;
 
         for (let i = 0; i < this.ln; i++) {
-          
+
           let yy: number = parseInt(resp.body[i].dateTime.substring(0, 4), 10);
-          let mm: number = parseInt(resp.body[i].dateTime.substring(5, 7), 10); 
+          let mm: number = parseInt(resp.body[i].dateTime.substring(5, 7), 10);
           let dd: number = parseInt(resp.body[i].dateTime.substring(8, 10), 10);
           let hh: number = parseInt(resp.body[i].dateTime.substring(11, 13), 10);
           let mi: number = parseInt(resp.body[i].dateTime.substring(14, 16), 10);
           let ss: number = parseInt(resp.body[i].dateTime.substring(17, 19), 10);
-        
-          let date: Date = new Date(yy, mm, dd, hh, mi, ss);
-         
+          
+          let calculated: number = (ss / 60) + mi;
+          calculated = (calculated / 60) + hh;
+          calculated = (calculated / 24) + dd;
+          calculated = (calculated / 30) + mm;
+          calculated = (calculated / 12) + yy;
+          
+          console.log(calculated)
+          
+
           this.tmp = new Sighting(resp.body[i].id,
-                                  resp.body[i].dateTime,
-                                  resp.body[i].description,
-                                  resp.body[i].species,
-                                  resp.body[i].count);
+            resp.body[i].dateTime,
+            resp.body[i].description,
+            resp.body[i].species,
+            resp.body[i].count,
+            calculated);
           this.results.push(this.tmp);
           
+          
+          this.sort();
+
         }
-        
+
       });
-  }
-  
-  sort() {
     
+    
+  }
+
+  sort() {
+    console.log('sorting')
+    for (let i = 0; i < this.results.length; i++) {
+      for (let j = i; j > 0; j--) {
+        if (this.results[j].getCalculated < this.results[j - 1].getCalculated) {
+          let tmp = this.results[j];
+          this.results[j] = this.results[j - 1];
+          this.results[j - 1] = tmp;
+        }
+      }
+    }
   }
 }
 
@@ -60,8 +82,9 @@ export class Sighting {
     private dateTime: string,
     private description: string,
     private species: string,
-    private count: number) {
-  
+    private count: number,
+    private calculated: number) {
+
   }
 
   get getId(): number {
@@ -82,5 +105,9 @@ export class Sighting {
 
   get getCount(): number {
     return this.count;
+  }
+  
+  get getCalculated(): number {
+    return this.calculated;
   }
 }
