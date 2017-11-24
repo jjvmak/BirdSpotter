e990f14570705c64ada84b85fbc6e2194f0aa0cd
Joonas Mäkinen <makinen.jns@gmail.com>
2017-11-24 17:26:51 +0200

    Implementing routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import { SightingsComponent } from './sightings/sightings.component';
+import { InputComponent } from './input/input.component';
+
+const appRoutes: Routes = [
+  { path: 'input', component: InputComponent },
+  { path: 'sightings', component: SightingsComponent },
+  { path: '', component: SightingsComponent}
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SightingsComponent
+    SightingsComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot( appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
